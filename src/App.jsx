35dcbbd6bda7f5750eb1fca789fef41b9e4a5677
boryana-k <boryana.mk@outlook.com>
2024-09-28 14:11:49 +0300

    tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import './App.css'
 import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import Home from './Pages/Home';
 import {NextUIProvider} from "@nextui-org/react";
-import { supabase } from './createClient.js'
-import { useEffect, useState } from 'react';
 
 function App() {
   return (
