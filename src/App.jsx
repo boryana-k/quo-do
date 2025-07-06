a69e8b1e097c8ea1bfde887faf6d4b7ec0d0f97e
Boryana Krasteva <77883725+boryana-k@users.noreply.github.com>
2025-07-06 13:05:33 +0300

    tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import {HeroUIProvider, ToastProvider} from "@heroui/react";
-import Welcome from './pages/Welcome';
+// import Welcome from './pages/Welcome';
 import Home from './pages/Home';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <ToastProvider />
       <Routes>
         <Route path="/" exact element={<Home />}></Route>
-        <Route path="/welcome" exact element={<Welcome />}></Route>
+        {/* <Route path="/welcome" exact element={<Welcome />}></Route> */}
       </Routes>
     </HeroUIProvider>
   )
